e1c1a4eec04fb9b78946fd3bce8b92e779c1c3f2
Martin Williams <martinwork@users.noreply.github.com>
2020-12-11 13:13:20 +0000

    Update sdk_config to support all services including HF2

--- sdk_config/sdk_config.h
+++ sdk_config/sdk_config.h
@@ -4539,12 +4539,12 @@
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
 #ifndef NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE
-#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 0x720
+#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 0x7D0
 #endif
 
 // <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
 #ifndef NRF_SDH_BLE_VS_UUID_COUNT
-#define NRF_SDH_BLE_VS_UUID_COUNT 4
+#define NRF_SDH_BLE_VS_UUID_COUNT 6
 #endif
 
 // <q> NRF_SDH_BLE_SERVICE_CHANGED  - Include the Service Changed characteristic in the Attribute Table.
